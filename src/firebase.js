fd86e2cddc607360624ceb65c2a105608ceb3ebf
jsso16 <wjsthwls24@daum.net>
2022-03-30 20:06:02 +0900

    Add Firebase files Firebase 설치 및 SDK 파일 추가

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeFPr3jo06e_GjgeSxAANnIjapjE6gOeE",
+  authDomain: "nwitter-9b353.firebaseapp.com",
+  projectId: "nwitter-9b353",
+  storageBucket: "nwitter-9b353.appspot.com",
+  messagingSenderId: "887412301599",
+  appId: "1:887412301599:web:d06abd2dad56e73bbeda2a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
