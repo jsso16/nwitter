420ab56d80ba345f733791a3acd2d22deada81ad
jsso16 <wjsthwls24@daum.net>
2022-03-31 16:42:46 +0900

    Add .env file .env 파일을 통한 비밀키 숨김 처리

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeFPr3jo06e_GjgeSxAANnIjapjE6gOeE",
-  authDomain: "nwitter-9b353.firebaseapp.com",
-  projectId: "nwitter-9b353",
-  storageBucket: "nwitter-9b353.appspot.com",
-  messagingSenderId: "887412301599",
-  appId: "1:887412301599:web:d06abd2dad56e73bbeda2a"
+  apiKey: "process.env.REACT_APP_API_KEY",
+  authDomain: "process.env.REACT_APP_AUTH_DOMAIN",
+  projectId: "process.env.REACT_APP_PROJECT_ID",
+  storageBucket: "process.env.REACT_APP_STORAGE_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_MESSAGING_SENDER_ID",
+  appId: "process.env.REACT_APP_APP_ID"
 };
 
 // Initialize Firebase
