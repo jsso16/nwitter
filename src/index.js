3475408325e09c8ca44e7fe60201d6f05d9c446f
jsso16 <wjsthwls24@daum.net>
2022-03-31 12:19:07 +0900

    Check Firebase Firebase 동작 확인

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import Firebase from './firebase';
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+console.log(Firebase);
 
 ReactDOM.render(
   <React.StrictMode>
