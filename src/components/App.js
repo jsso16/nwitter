0dc2927a0350c6a34a6d5286a6cf9d845792aa17
jsso16 <wjsthwls24@daum.net>
2022-04-28 20:25:30 +0900

    Edit App.js 로그인 처리 완료 지연 시간 및 유저 상태 변화 확인

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // setInterval(() => console.log(authService.currentUser), 2000);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => console.log(user));
+  }, []);
 
   return (
     <>
