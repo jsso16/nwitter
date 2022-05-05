27606382b965bb3434f3c9cca27b7819343b49f4
jsso16 <wjsthwls24@daum.net>
2022-05-05 22:25:06 +0900

    Edit Router.js & Profile.js useHistory를 이용한 주소 이동 처리

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,18 @@
 import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
 const Profile = () => {
-  const onLogOutClick = () => authService.signOut();
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
 
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
-};
+}
 
 export default Profile;
