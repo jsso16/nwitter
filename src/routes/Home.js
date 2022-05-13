d2f078c7bf2cdf23788e43b38d404e603d7f8986
jsso16 <wjsthwls24@daum.net>
2022-05-13 10:11:37 +0900

    Edit Home.js 문서 읽어오는 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,18 @@
 import { dbService } from "fbase";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+
+  const getNweets = async() => {
+    const dbNweets = await dbService.collection("nweets").get();
+    // console.log(dbNweets);
+    dbNweets.forEach((document) => console.log(document.data()));
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async(event) => {
     event.preventDefault();
