dca26fa6cde33f3efd89197265f1bac8bba051c7
jsso16 <wjsthwls24@daum.net>
2022-05-13 13:34:40 +0900

    Edit Home.js 게시물 목록 생성을 위한 코드 추가 및 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,16 +3,21 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
 
   const getNweets = async() => {
     const dbNweets = await dbService.collection("nweets").get();
-    // console.log(dbNweets);
-    dbNweets.forEach((document) => console.log(document.data()));
+
+    dbNweets.forEach((document) => 
+      setNweets((prev) => [document.data(), ...prev])
+    );
   };
 
   useEffect(() => {
     getNweets();
   }, []);
+
+  console.log(nweets);
 
   const onSubmit = async(event) => {
     event.preventDefault();
