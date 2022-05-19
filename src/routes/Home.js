f9724a09a133c076b1bd6ab95d822ee4ccf3b239
jsso16 <wjsthwls24@daum.net>
2022-05-19 15:56:11 +0900

    Edit Home.js onSnapshot 함수 적용

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,13 @@ const Home = ({ userObj }) => {
   const [nweets, setNweets] = useState([]);
 
   useEffect(() => {
-
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    });
   }, []);
 
   const onSubmit = async(event) => {
