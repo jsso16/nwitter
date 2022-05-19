15437bc28553ca6dac56971568f62a182a5704bf
jsso16 <wjsthwls24@daum.net>
2022-05-19 20:37:59 +0900

    Edit Nweet.js & Home.js 작성자 본인의 게시물 삭제 및 수정 기능 구현

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,9 +1,13 @@
-const Nweet = ({ nweetObj }) => {
+const Nweet = ({ nweetObj, isOwner }) => {
   return (
     <div>
       <h4>{nweetObj.text}</h4>
-      <button>Delete Nweet</button>
-      <button>Edit Nweet</button>
+      {isOwner && (
+        <>
+          <button>Delete Nweet</button>
+          <button>Edit Nweet</button>
+        </>
+      )}
     </div>
   )
 }
